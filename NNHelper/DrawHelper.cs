f6b2c14b0f880135e3fc050cd60671e53702a67a
Vladimir Ulihin <vadash@gmail.com>
2019-03-20 00:03:44 +0300

    Drawing refactoring

--- NNHelper/DrawHelper.cs
+++ NNHelper/DrawHelper.cs
@@ -17,7 +17,6 @@ namespace NNHelper
             mainWnd = new GraphicWindow(settings.SizeX, settings.SizeY);
         }
 
-
         public void DrawPlaying(Point curMousePos, string selectedObject, Settings settings, IEnumerable<YoloItem> items,
             bool firemode)
         {
@@ -25,37 +24,34 @@ namespace NNHelper
             mainWnd.window.Y = curMousePos.Y - s.SizeY / 2;
             mainWnd.graphics.BeginScene();
             mainWnd.graphics.ClearScene();
-
-            if (s.DrawAreaRectangle)
-                mainWnd.graphics.DrawRectangle(mainWnd.graphics.csb, 0, 0, s.SizeX, s.SizeY, 2);
-
-            mainWnd.graphics.FillRectangle(firemode ? mainWnd.graphics.csfmb : mainWnd.graphics.csb,
-                Rectangle.Create(s.SizeX / 2, s.SizeY / 2, 4, 4));
-
-            //draw main text
-            if (s.DrawText)
-                mainWnd.graphics.WriteText(
-                    $"FPS {mainWnd.graphics.FPS}");
-
+            // draw area
+            mainWnd.graphics.DrawRectangle(mainWnd.graphics.blueBrush, 0, 0, s.SizeX, s.SizeY, 2);
+            // draw crossfire
+            mainWnd.graphics.FillRectangle(firemode ? mainWnd.graphics.redBrush : mainWnd.graphics.blueBrush,
+                Rectangle.Create(s.SizeX / 2 - 2, s.SizeY / 2 - 2, 4, 4));
+            //mainWnd.graphics.WriteText(
+            //    $"FPS {mainWnd.graphics.FPS}");
+            // draw targets
             foreach (var item in items) DrawItem(item);
-
             mainWnd.graphics.EndScene();
         }
 
         private void DrawItem(YoloItem item)
         {
-            var body = Rectangle.Create(
-                item.X + Convert.ToInt32(item.Width / 6f),
-                item.Y + Convert.ToInt32(item.Height / 6f),
-                Convert.ToInt32(item.Width / 1.5f),
-                Convert.ToInt32(item.Height / 2f));
-            mainWnd.graphics.DrawRectangle(mainWnd.graphics.bcb, body, 2);
-
-            //mainWnd.graphics.DrawRectangle(mainWnd.graphics.hcb, GameOverlay.Drawing.Rectangle.Create(item.X, item.Y, item.Width, item.Height), 2);
-            //mainWnd.graphics.DrawCrosshair(mainWnd.graphics.bcb, body.Left + body.Width / 2,
-            //body.Top + body.Height / 2 + Convert.ToInt32(1 * shooting), 2, 2, CrosshairStyle.Cross);
-            //mainWnd.graphics.DrawLine(mainWnd.graphics.bcb, s.SizeX / 2, s.SizeY / 2, body.Left + body.Width / 2,
-            //body.Top + body.Height / 2 + Convert.ToInt32(1 * shooting), 2);
+            var head = Rectangle.Create(item.X + Convert.ToInt32(item.Width / 2.9), item.Y,
+                Convert.ToInt32(item.Width / 3), item.Height / 7);
+            var body = Rectangle.Create(item.X + Convert.ToInt32(item.Width / 6), item.Y + item.Height / 6,
+                Convert.ToInt32(item.Width / 1.5f), item.Height / 3);
+            // aim line
+            mainWnd.graphics.DrawLine(mainWnd.graphics.blueBrush,
+                s.SizeX / 2f,
+                s.SizeY / 2f,
+                body.Left + body.Width / 2,
+                body.Top + body.Height / 2,
+                2);
+            // body + head
+            mainWnd.graphics.DrawRectangle(mainWnd.graphics.redBrush, body, 2);
+            mainWnd.graphics.DrawRectangle(mainWnd.graphics.blueBrush, head, 2);
         }
 
         public void DrawDisabled()
--- NNHelper/GraphicsHelper.cs
+++ NNHelper/GraphicsHelper.cs
@@ -5,47 +5,46 @@ namespace NNHelper
     public class GraphicsEx : Graphics
     {
         public static readonly Point StartPoint = new Point(0, 0);
-        public SolidBrush acb;
-        public SolidBrush bcb;
-        public SolidBrush csb;
-        public SolidBrush csfmb;
+        public SolidBrush areaBrush;
+        public SolidBrush bodyBrush;
+        public SolidBrush blueBrush;
+        public SolidBrush redBrush;
 
         private Font defaultFont;
-        public SolidBrush hcb;
-        public SolidBrush tfb;
+        public SolidBrush headBrush;
+        public SolidBrush textBrush;
         public static Color AreaColor { get; set; } = new Color(0, 255, 0, 10);
         public static Color TextColor { get; set; } = new Color(120, 255, 255);
         public static Color BodyColor { get; set; } = new Color(0, 255, 0, 80);
         public static Color HeadColor { get; set; } = new Color(255, 0, 0, 80);
 
         public static string DefaultFontStr { get; set; } = "Arial";
-        public static int DefaultFontSize { get; set; } = 12;
+        public static int DefaultFontSize { get; set; } = 14;
 
 
         public new void Setup()
         {
             base.Setup();
-
             defaultFont = CreateFont(DefaultFontStr, DefaultFontSize);
-            acb = AreaColor.GetSolidBrush(this);
-            tfb = TextColor.GetSolidBrush(this);
-            csb = CreateSolidBrush(Color.Blue);
-            csfmb = CreateSolidBrush(Color.Red);
-            hcb = HeadColor.GetSolidBrush(this);
-            bcb = BodyColor.GetSolidBrush(this);
+            areaBrush = AreaColor.GetSolidBrush(this);
+            textBrush = TextColor.GetSolidBrush(this);
+            blueBrush = CreateSolidBrush(Color.Blue);
+            redBrush = CreateSolidBrush(Color.Red);
+            headBrush = HeadColor.GetSolidBrush(this);
+            bodyBrush = BodyColor.GetSolidBrush(this);
         }
 
         public void WriteText(string text)
         {
-            DrawText(defaultFont, tfb, StartPoint, text);
+            DrawText(defaultFont, textBrush, StartPoint, text);
         }
     }
 
     public static class ColorHelper
     {
-        public static SolidBrush GetSolidBrush(this Color c, Graphics graphics)
+        public static SolidBrush GetSolidBrush(this Color color, Graphics graphics)
         {
-            return graphics.CreateSolidBrush(c);
+            return graphics.CreateSolidBrush(color);
         }
     }
 }
